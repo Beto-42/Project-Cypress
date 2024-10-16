f158c82df16899d24ac206d6dc971a88a8a851ec
Beto-42 <robertopd.ti@gmail.com>
2024-10-16 10:27:38 -0300

    first commit

---
+++ cypress/e2e/register.cy.js
@@ -0,0 +1,11 @@
+describe('template spec', () => {
+
+    it('passes', () => {
+    cy.visit('https://mantis-prova.base2.com.br/')
+    cy.get('#username').type('roberto_oliveira')
+    cy.get('.width-40').click()
+    cy.get('#password').type('teste24')
+    cy.get('.width-40').click()
+
+  })
+})
