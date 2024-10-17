2d5e91f3eede537f10d33b8f77fb8ecc0151354b
Beto-42 <robertopd.ti@gmail.com>
2024-10-17 20:31:38 -0300

    Fifth commit

--- cypress/e2e/AddNota.cy.js
+++ cypress/e2e/AddNota.cy.js
@@ -8,11 +8,11 @@ describe('Adicionando Nota', () => {
 
         cy.get(':nth-child(2) > a > .menu-icon').click()
         cy.get('tbody > :nth-child(1) > .column-id > a').click()
-        cy.get('#bugnote_add > .widget-header > .widget-toolbar').click()
+        cy.get(':nth-child(2) > .widget-box > .widget-header > .widget-toolbar').click()
         cy.get('.lbl').click()
         cy.get('#bugnote_text').type('TesteTesteTesteTesteTesteTesteTeste').click()
         cy.get('.widget-toolbox > .btn').click()
-        
+
 
     })
 
